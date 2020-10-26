b44e4784ff5482b249e4ffce34c9dc526ae45efa
Raz <73390462+RazLightYear@users.noreply.github.com>
2020-10-26 16:56:35 +0200

    Update Calculator.java

--- GUIpractice/src/Calculator.java
+++ GUIpractice/src/Calculator.java
@@ -23,113 +23,40 @@ public static void main(String[] args)
 
 class calculatorFrame extends JFrame implements ActionListener
 {
-	private JTextField t1;
-	private JButton addition,substraction,multiply,divide,equals,Clear,b1,b2,b3,b4,b5,b6,b7,b8,b9,b0;
-	private String s1,s2,s3;	
+	// TextField to present the calculations
+	private JTextField t1; 
+	
+	// strings to hold the values and operands
+	private String s1,s2,s3;
+	
+	// An array of buttons
+	private JButton [] buttons;
+	
+	// the buttons names
+	private String[] names= {"+","1","2","3","-","4","5","6","*","7","8","9","/","0","=","C"};		
 	
 	public calculatorFrame()
 	{
 		s1=s2=s3="";
-				
+		Font f=new Font("Arial",Font.BOLD,20);
+		buttons=new JButton[names.length];
+		
 		t1=new JTextField();
 		t1.setEditable(false);
 		t1.setBackground(Color.white);
-		t1.setPreferredSize(new Dimension(230,40));
-		Font f=new Font("Arial",Font.BOLD,20);
+		t1.setPreferredSize(new Dimension(230,40));		
 		t1.setFont(f);
 		add(t1);
 		
+		for(int i=0;i<names.length;i++)
+		{
+			buttons[i]=new JButton(names[i]);
+			buttons[i].setFont(f);
+			buttons[i].setPreferredSize(new Dimension(50,50));
+			add(buttons[i]);
+			buttons[i].addActionListener(this);
+		}
 		
-		addition=new JButton("+");
-		addition.setFont(f);
-		addition.setPreferredSize(new Dimension(50,50));
-		add(addition);
-		addition.addActionListener(this);
-		
-		
-		b1=new JButton("1");
-		b1.setPreferredSize(new Dimension(50,50));
-		b1.setFont(f);
-		add(b1);
-		b1.addActionListener(this);
-		b2=new JButton("2");
-		b2.setPreferredSize(new Dimension(50,50));
-		b2.setFont(f);
-		add(b2);
-		b2.addActionListener(this);
-		b3=new JButton("3");
-		b3.setPreferredSize(new Dimension(50,50));
-		b3.setFont(f);
-		add(b3);
-		b3.addActionListener(this);
-		
-		substraction=new JButton("-");
-		substraction.setFont(f);
-		substraction.setPreferredSize(new Dimension(50,50));
-		add(substraction);
-		substraction.addActionListener(this);
-		
-		b4=new JButton("4");
-		b4.setPreferredSize(new Dimension(50,50));
-		b4.setFont(f);
-		add(b4);
-		b4.addActionListener(this);
-		b5=new JButton("5");
-		b5.setPreferredSize(new Dimension(50,50));
-		b5.setFont(f);
-		add(b5);
-		b5.addActionListener(this);
-		b6=new JButton("6");
-		b6.setPreferredSize(new Dimension(50,50));
-		b6.setFont(f);
-		add(b6);
-		b6.addActionListener(this);
-		
-		multiply=new JButton("*");
-		multiply.setFont(f);
-		multiply.setPreferredSize(new Dimension(50,50));
-		add(multiply);
-		multiply.addActionListener(this);
-		
-		b7=new JButton("7");
-		b7.setPreferredSize(new Dimension(50,50));
-		b7.setFont(f);
-		add(b7);
-		b7.addActionListener(this);
-		b8=new JButton("8");
-		b8.setPreferredSize(new Dimension(50,50));
-		b8.setFont(f);
-		add(b8);
-		b8.addActionListener(this);
-		b9=new JButton("9");
-		b9.setPreferredSize(new Dimension(50,50));
-		b9.setFont(f);
-		add(b9);
-		b9.addActionListener(this);
-		
-		divide=new JButton("/");
-		divide.setFont(f);
-		divide.setPreferredSize(new Dimension(50,50));
-		add(divide);
-		divide.addActionListener(this);
-		
-		b0=new JButton("0");
-		b0.setPreferredSize(new Dimension(50,50));
-		b0.setFont(f);
-		add(b0);
-		b0.addActionListener(this);
-		
-		equals=new JButton("=");
-		equals.setPreferredSize(new Dimension(50,50));
-		equals.setFont(f);
-		add(equals);
-		equals.addActionListener(this);
-		
-		Clear=new JButton("C");
-		Clear.setPreferredSize(new Dimension(50,50));
-		Clear.setFont(f);
-		add(Clear);
-		Clear.addActionListener(this);
 		
 		FlowLayout lay=new FlowLayout();
 		lay.setAlignment(FlowLayout.LEFT);
@@ -148,16 +75,16 @@ class calculatorFrame extends JFrame implements ActionListener
 		
 		if(s.charAt(0)>='0' && s.charAt(0)<='9')
 			{
-			if(!s2.equals("")) // if s2 is still "" that means that no operand was pressed. 
-				               // if s2 is not "" anymore that means that an operand was pressed and the rest of the numbers needs to go inside the second number
-			{
-		 		s3=s3+s;
-			}
+				if(!s2.equals("")) // if s2 is still "" that means that no operand was pressed. 
+					               // if s2 is not "" anymore that means that an operand was pressed and the rest of the numbers needs to go inside the second number
+				{
+					s3=s3+s;
+				}
 				
-		else
-			{
-				s1=s1+s;
-			}
+				else
+				{
+					s1=s1+s;
+				}
 				
 				t1.setText(s1+s2+s3);
 			}
@@ -202,8 +129,8 @@ class calculatorFrame extends JFrame implements ActionListener
 			}
 			
 			else 
-			{
-			int tmp;
+				{
+				int tmp;
 				if (s2.equals("+")) 
 					tmp = (Integer.parseInt(s1) + Integer.parseInt(s3)); 
                 		else if (s2.equals("-")) 
@@ -213,10 +140,10 @@ class calculatorFrame extends JFrame implements ActionListener
                 		else
                 			tmp = (Integer.parseInt(s1) * Integer.parseInt(s3)); 
                   
-            		s1 = Integer.toString(tmp);               
-                	s2 = s;                   
-                	s3 = ""; 
-             		} 
+                		s1 = Integer.toString(tmp);               
+                		s2 = s;                   
+                		s3 = ""; 
+         		   	} 
 		}		
 	}			
 }
